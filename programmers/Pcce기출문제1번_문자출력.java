5b478f5e6cfcf649c9781ff417ea087672d84ae5
suuny0321 <121182058+suuny0321@users.noreply.github.com>
2024-10-01 21:34:09 +0900

    문자 출력 문제 해결

---
+++ programmers/Pcce기출문제1번_문자출력.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Pcce기출문제1번_문자출력 {
+    public static void main(String[] args) {
+
+
+        String message = "Let's go!"; // 메시지를 변수에 저장
+
+        System.out.println("3\n2\n1"); // 숫자 3, 2, 1을 줄바꿈 문자 \n을 사용해 출력
+
+        System.out.println(message);  // 메시지 출력
+    }
+    }
+
+
+
